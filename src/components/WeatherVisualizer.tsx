9c5ebaa33a12503af0c2adddba08ed7017b99416
Elias Peteri <elias.peteri@hotmail.com>
2025-04-24 18:39:09 +0300

    refactor(WeatherVisualizer): remove unused socketManager import

--- src/components/WeatherVisualizer.tsx
+++ src/components/WeatherVisualizer.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import socketManager from '../networking/socket';
 import useStore from '../store';
 import Precipitation from './Precipitation';
 
