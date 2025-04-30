35e5630ef7bf0f4408d7ec80faf4c3242e48268a
Elias Peteri <elias.peteri@hotmail.com>
2025-04-30 12:15:57 +0300

    fix(tests): correct expected result for add(1, 1) in sum test

--- assembly/add.test.ts
+++ assembly/add.test.ts
@@ -7,6 +7,6 @@ import { add } from './add';
 
 test('sum', () => {
   expect(add(1, 2)).equal(3);
-  expect(add(1, 1)).equal(3);
+  expect(add(1, 1)).equal(2); // Fixed: 1+1=2, not 3
 });
 endTest(); // Don't forget it!
