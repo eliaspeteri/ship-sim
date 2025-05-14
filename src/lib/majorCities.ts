adc7be2f4fd064ee1a4336c5fbae2357c3b9bb4d
Elias Peteri <elias.peteri@hotmail.com>
2025-05-14 12:34:50 +0300

    feat: add majorCities data for improved globe visualization

---
+++ src/lib/majorCities.ts
@@ -0,0 +1,172 @@
+export const majorCities = [
+  {
+    name: 'Helsinki',
+    lat: 60.1695,
+    lon: 24.9354,
+  },
+  {
+    name: 'New York',
+    lat: 40.7128,
+    lon: -74.006,
+  },
+  {
+    name: 'Tokyo',
+    lat: 35.6895,
+    lon: 139.6917,
+  },
+  {
+    name: 'London',
+    lat: 51.5074,
+    lon: -0.1278,
+  },
+  {
+    name: 'Paris',
+    lat: 48.8566,
+    lon: 2.3522,
+  },
+  {
+    name: 'Berlin',
+    lat: 52.52,
+    lon: 13.405,
+  },
+  {
+    name: 'Sydney',
+    lat: -33.8688,
+    lon: 151.2093,
+  },
+  {
+    name: 'Cairo',
+    lat: 30.0444,
+    lon: 31.2357,
+  },
+  {
+    name: 'Moscow',
+    lat: 55.7558,
+    lon: 37.6173,
+  },
+  {
+    name: 'Rio de Janeiro',
+    lat: -22.9068,
+    lon: -43.1729,
+  },
+  {
+    name: 'Cape Town',
+    lat: -33.9249,
+    lon: 18.4241,
+  },
+  {
+    name: 'Mexico City',
+    lat: 19.4326,
+    lon: -99.1332,
+  },
+  {
+    name: 'Bangkok',
+    lat: 13.7563,
+    lon: 100.5018,
+  },
+  {
+    name: 'Istanbul',
+    lat: 41.0082,
+    lon: 28.9784,
+  },
+  {
+    name: 'Buenos Aires',
+    lat: -34.6037,
+    lon: -58.3816,
+  },
+  {
+    name: 'Lagos',
+    lat: 6.5244,
+    lon: 3.3792,
+  },
+  {
+    name: 'Seoul',
+    lat: 37.5665,
+    lon: 126.978,
+  },
+  {
+    name: 'Madrid',
+    lat: 40.4168,
+    lon: -3.7038,
+  },
+  {
+    name: 'Rome',
+    lat: 41.9028,
+    lon: 12.4964,
+  },
+  {
+    name: 'Toronto',
+    lat: 43.6511,
+    lon: -79.347015,
+  },
+  {
+    name: 'Lima',
+    lat: -12.0464,
+    lon: -77.0428,
+  },
+  {
+    name: 'Kuala Lumpur',
+    lat: 3.139,
+    lon: 101.6869,
+  },
+  {
+    name: 'Santiago',
+    lat: -33.4489,
+    lon: -70.6693,
+  },
+  {
+    name: 'Athens',
+    lat: 37.9838,
+    lon: 23.7275,
+  },
+  {
+    name: 'Hanoi',
+    lat: 21.0285,
+    lon: 105.8542,
+  },
+  {
+    name: 'Colombo',
+    lat: 6.9271,
+    lon: 79.9585,
+  },
+  {
+    name: 'Nairobi',
+    lat: -1.2864,
+    lon: 36.8172,
+  },
+  {
+    name: 'Addis Ababa',
+    lat: 9.03,
+    lon: 38.74,
+  },
+  {
+    name: 'Casablanca',
+    lat: 33.5731,
+    lon: -7.5898,
+  },
+  {
+    name: 'Honolulu',
+    lat: 21.3069,
+    lon: -157.8583,
+  },
+  {
+    name: 'Wellington',
+    lat: -41.2865,
+    lon: 174.7762,
+  },
+  {
+    name: 'Oslo',
+    lat: 59.9139,
+    lon: 10.7522,
+  },
+  {
+    name: 'Stockholm',
+    lat: 59.3293,
+    lon: 18.0686,
+  },
+  {
+    name: 'Alert',
+    lat: 82.5018,
+    lon: -62.3481,
+  },
+];
